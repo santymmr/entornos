1c082b64aa58a2e8971f7b3763ed62313bac0d02
dam1 <dam1@lliurexadmin-optiplex5070>
2023-03-28 18:59:23 +0200

    Holaa

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dam1
+ *
+ */
+module repository {
+}
---
+++ src/repository/prueba.java
@@ -0,0 +1,9 @@
+package repository;
+
+import java.util.Scanner;
+
+public class prueba {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	}
+}
